ded6cafc737f50955bf474d776fda39c574daaeb
Stephen <ss083193@gmail.com>
2014-10-26 22:19:51 -0400

    issue51: save checkpoint

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,9 +1,29 @@
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication',
-	function($scope, Authentication) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication', 'Committees', '$filter',
+	function($scope, Authentication, Committees, $filter) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+		$scope.currentUser = Authentication.user;
+
+		$scope.role = {
+			'admin' : false,
+			'user' : false,
+		};
+
+		$scope.getRole = function() {
+
+			if($filter('lowercase')($scope.currentUser.role) === 'admin')
+				$scope.role.admin = true;
+			if($filter('lowercase')($scope.currentUser.role) === 'user')
+				$scope.role.user = true;
+		};
+
+		// Find a List of Committees
+		Committees.Committees.query().$promise.then(function(data) {
+			$scope.committees = data;
+			$log.info('List of Committees Loaded');
+		});
 	}
 ]);
