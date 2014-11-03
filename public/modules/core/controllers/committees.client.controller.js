3c2ebda76f0c7cc05c8d31c2e8a31f8bd4c151d5
Stephen <ss083193@gmail.com>
2014-11-03 12:13:38 -0500

    Modified lastCommittee function in CommitteesCtrl

--- public/modules/core/controllers/committees.client.controller.js
+++ public/modules/core/controllers/committees.client.controller.js
@@ -97,10 +97,12 @@ angular.module('core').controller('CommitteesCtrl', ['$scope', '$log', '$q', '$s
 		// Sprint 2 Temp fix
 		$scope.lastCommittee = function(commMembers) {
 			$log.debug('Entered last committee');
-			$scope.currentCommitteeObjects = {
-				'committee' : $scope.committee,
-				'members' : commMembers
-			};
+			if(!($scope.committee._id === $scope.currentCommitteeObjects.committee._id)) {
+				$scope.currentCommitteeObjects = {
+					'committee' : $scope.committee,
+					'members' : commMembers
+				};
+			}
 			$log.debug('currentCommitteeObject');
 			$log.debug($scope.currentCommitteeObjects);
 		};
