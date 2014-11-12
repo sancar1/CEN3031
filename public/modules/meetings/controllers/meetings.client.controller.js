4c4ee38e4054e07d0a57e872360bd340c27fb066
Zachary Burke <zburke1@ufl.edu>
2014-11-11 21:27:05 -0500

    Inserted date and time picker. Not linked up and CSS needs cleaning

--- public/modules/meetings/controllers/meetings.client.controller.js
+++ public/modules/meetings/controllers/meetings.client.controller.js
@@ -103,5 +103,79 @@ angular.module('meetings').controller('MeetingsController', ['$scope', '$statePa
 			//	$scope.getMembers();	
 			});
 		};
+	//Start of Date Picker Code
+	
+	
+	$scope.today = function() {
+	    $scope.dt = new Date();
+	  };
+	  $scope.today();
+
+	  $scope.clear = function () {
+	    $scope.dt = null;
+	  };
+
+	  // Disable weekend selection
+	  // $scope.disabled = function(date, mode) {
+ // 	    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+ // 	  };
+
+	  $scope.toggleMin = function() {
+	    $scope.minDate = $scope.minDate ? null : new Date();
+	  };
+	  $scope.toggleMin();
+
+	  $scope.open = function($event) {
+	    $event.preventDefault();
+	    $event.stopPropagation();
+
+	    $scope.opened = true;
+	  };
+
+	  $scope.dateOptions = {
+	    formatYear: 'yy',
+	    startingDay: 1
+	  };
+
+	  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+	  $scope.format = $scope.formats[3];
+		
+		
+	//End of Date Picker Code	
+	//Start Time Picker Code
+	
+   $scope.myStartTime = new Date();
+ 	$scope.myEndTime = new Date();
+    $scope.hstep = 1;
+    $scope.mstep = 15;
+
+    // $scope.options = {
+//       hstep: [1, 2, 3],
+//       mstep: [1, 5, 10, 15, 25, 30]
+//     };
+
+    $scope.ismeridian = true;
+    // $scope.toggleMode = function() {
+//       $scope.ismeridian = ! $scope.ismeridian;
+//     };
+
+    $scope.update = function() {
+      var d = new Date();
+      d.setHours( 14 );
+      d.setMinutes( 0 );
+      $scope.mytime = d;
+    };
+
+    $scope.changed = function () {
+      console.log('Time changed to: ' + $scope.mytime);
+    };
+
+    $scope.clear = function() {
+      $scope.mytime = null;
+    };
+  //End Time Picker Code
+	
+	
+	
 	}
 ]);
