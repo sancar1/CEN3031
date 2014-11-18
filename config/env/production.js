df908d4f29fb82d98fd89d4f7f1e626cac6e8737
Clay Brooks <brks9622@gmail.com>
2014-11-17 20:49:40 -0500

    switched databases

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: 'mongodb://master:master12345@ds035270.mongolab.com:35270/master',
+	db: db: 'mongodb://cen3031:cen3031@dogen.mongohq.com:10080/CEN3031',
 	app: {
 		title: 'CACTUS Committee Manager - Development Environment'
 	},
